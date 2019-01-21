da156baeb40de0b5688536e7cc6580ed5c72cbdb
saint12maloj <saint12maloj@gmail.com>
2019-01-21 13:46:55 +0300

    Неверное добавление бойца в отряд

--- Caravans/Program.cs
+++ Caravans/Program.cs
@@ -20,7 +20,21 @@ namespace Caravans
                 {
                     Console.WriteLine($"{i.Id}-{i.FactionName} ");
                 }
-                Console.WriteLine("Choose a faction to look through it's warriors...");
+                Console.WriteLine("Choose a faction to look through it's squads...");
+                var factionId_0 = Int32.Parse(Console.ReadLine());
+                if (factionId_0 != 0)
+                {
+                    var result_0 = context.Factions.Find(factionId_0);
+                    foreach (var i in result_0.Squads)
+                    {
+                        Console.WriteLine($"{i.Id}-Squad {i.SquadName} with it's treasury of {i.MasterCard} $;");
+                    }
+                }
+                    Console.WriteLine("Choose a faction to look through the warriors to buy...");
+                foreach (var i in faction)
+                {
+                    Console.WriteLine($"{i.Id}-{i.FactionName} ");
+                }
 
                 var factionId = Int32.Parse(Console.ReadLine());
                 if (factionId != 0)
@@ -33,6 +47,17 @@ namespace Caravans
                         {
                             Console.WriteLine($"{i.Id}-{i.WarriorName} with {i.HP} HP, {i.Price} $ cost, {i.AttackStrength} points of attack power and {i.BlockStrength} points security abilities.");
                         }
+                    }
+                }
+                Console.WriteLine("Choose a warrior to buy...");
+                var warriorId = Int32.Parse(Console.ReadLine());
+                if (warriorId != 0)
+                {
+                    var result_1 = context.Warriors.Find(warriorId);
+                    if (result_1 != null)
+                    {
+                        var chosenWarrior = context.Warriors.Where(x => x.Id == warriorId).Select(x => x).FirstOrDefault();
+                        context.Squads.Add(chosenWarrior);
                     }
                 }
 
